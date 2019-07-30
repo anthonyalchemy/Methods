b5c3b6d8d13363f0ca8926503525e80991527e88
anthonyalchemy <anthony@alchemytechs.com>
2019-07-30 16:08:37 +0100

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 33);
+            SayHi("John", 19);
+            SayHi("Tom", 56);
+            //Calling the method from below mulitple times. 
+
+            Console.ReadLine();
+        }
+
+       
+
+        static void SayHi(String name, int age) //task of method is to say hi to user //Parameters adding in name from user 
+        {
+            Console.WriteLine("Hello " + name + " you are "  + age);
+
+        }
+    }
+}
